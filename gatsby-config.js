3df02a465888dfe1cf28e1620dbc3a1987a8c90f
NicolasSchwandner <nicolas.schwandner@wint.se>
2023-12-17 19:04:30 +0100

    Gatsby tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const config = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
 
 module.exports = config;
