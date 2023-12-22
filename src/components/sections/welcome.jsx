9982813ee6f79313b78a4dc54fc8e83f9980f36e
NicolasSchwandner <nicolas.schwandner@wint.se>
2023-12-22 17:40:38 +0100

    WIP Call to action finished

--- src/components/sections/welcome.jsx
+++ src/components/sections/welcome.jsx
@@ -7,6 +7,8 @@ import {
   welcomeTitle,
   welcomePhoneNumber,
   noDefaultUnderline,
+  sectionContainer,
+  callToActionText,
 } from "./welcome.module.css";
 import profilePicture from "../../images/profile.png";
 import { iconButtonSecondary } from "../iconButtons/iconButton.module.css";
@@ -20,13 +22,30 @@ const Welcome = () => {
       <div style={{ "--background-image": `url(${profilePicture})` }} className={`${backgroundImage} ${profileImage}`}>
         <p className={welcomeTitle}>Willkommen bei Hellermann Transporte e.K.</p>
       </div>
-      <div>
+      <div className={sectionContainer}>
         <p className={welcomeText}>
           Wir sind ein kleines Familienunternehmen, welches sich auf diverse Arbeiten rund um Hausbau, -abriss und
           Entsorgung spezialisiert hat.
           <br />
           <br />
           Bei Fragen bitte einfach anrufen!
+        </p>
+        <a
+          href={`tel:${phoneNumber}`}
+          className={`icon-button ${iconButtonSecondary} ${welcomePhoneNumber} ${noDefaultUnderline}`} //TODO fix underline on hover
+        >
+          <span className={`material-symbols-outlined ${iconButtonSecondary}`} style={{ fontSize: "30px" }}>
+            call
+          </span>
+          {phoneNumber}
+        </a>
+      </div>
+      <div>Bilder</div>
+      <div className={sectionContainer}>
+        <p className={callToActionText}>
+          Fragen?
+          <br />
+          Einfach anrufen!
         </p>
         <a
           href={`tel:${phoneNumber}`}
